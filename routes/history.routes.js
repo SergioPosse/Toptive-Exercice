b15de5580db0bd7b1bfbf3ad2721a1cfe00ce3bc
Sergio Posse <sergiodavidposse@gmail.com>
2020-05-06 17:50:17 -0300

    testing get and post with postman

--- routes/history.routes.js
+++ routes/history.routes.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+const History = require('../models/History'); //link the model file
 
-router.get('/', (req, res) => { 
-    res.render('index.ejs', {titulo: "Calculadora REST"}); //no need to specificate the views path, already did in settings with path.join
+
+router.get('/', async (req, res) => { 
+    const history = await  History.find();
+    res.json(history);
+});
+
+router.post('/', async (req, res) => { //post
+    console.log(req.body);
 });
 
 module.exports = router;
